d05c9ceb547ac7b8709186feb88b7d223085f7c5
Labidox <44317793+Labidox@users.noreply.github.com>
2019-06-15 13:10:07 +0200

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,41 @@
+import { API_URL } from "../constants";
+
+export const ACTION_TYPES = {
+  PRODUCTS_FETCH: "PRODUCTS_FETCH",
+  PRODUCTS_FETCH_SUCCESS: "PRODUCTS_FETCH_SUCCESS",
+  PRODUCTS_FETCH_ERROR: "PRODUCTS_FETCH_ERROR",
+  ADD_TO_CART: "ADD_TO_CART"
+};
+
+export const fetchProducts = () => ({
+  type: ACTION_TYPES.PRODUCTS_FETCH
+});
+
+export const fetchProductsSuccess = products => ({
+  type: ACTION_TYPES.PRODUCTS_FETCH_SUCCESS,
+  products
+});
+
+export const fetchProductsError = () => ({
+  type: ACTION_TYPES.PRODUCTS_FETCH_ERROR
+});
+
+export const getProducts = () => {
+  return async dispatch => {
+    try {
+      dispatch(fetchProducts());
+      const response = await fetch(API_URL);
+      const products = await response.json();
+
+      dispatch(fetchProductsSuccess(products));
+    } catch (e) {
+      dispatch(fetchProductsError());
+    }
+  };
+};
+
+export const addToCart = product => dispatch =>
+  dispatch({
+    type: ACTION_TYPES.ADD_TO_CART,
+    product
+  });
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "../Container/index";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <Container>
+        <p>
+          Shop © 2020 <br /> Designed by Andrzej
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
